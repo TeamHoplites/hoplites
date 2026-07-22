41d9a809037c48ecc789d3537859689c92dbef1f
sizh007 <sivezhmb@gmail.com>
2026-07-22 15:46:41 +0530

    chore: configure deployment settings for hoplites repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// GitHub Pages deployment config for TeamHoplites/hoplites
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
